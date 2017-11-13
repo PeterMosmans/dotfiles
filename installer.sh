500751aef712353271b9e83594e9066ab82157d1
Peter Mosmans <support@go-forward.net>
2017-11-13 20:05:21 +1000

    Make .zsh as well as .zshenv OS-specific files

--- installer.sh
+++ installer.sh
@@ -13,9 +13,9 @@
 # Destination where the files need to be installed
 DESTINATIONPATH=~
 # Files and directories which need to be installed
-SOURCEFILES=".aliases .bashrc .emacs.d/init.el .gitconfig .tmux.conf .zshrc"
+SOURCEFILES=".aliases .bashrc .emacs.d/init.el .gitconfig .tmux.conf"
 # OS-specificy (uname -o) files and directories
-OSSPECIFIC=".zshenv .ssh/config"
+OSSPECIFIC=".ssh/config .zshrc .zshenv"
 
 
 ## Don't change anything below this line
