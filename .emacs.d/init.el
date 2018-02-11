760e2a91c4d67cb38d381ac85a381c197c073ad8
Peter Mosmans <support@go-forward.net>
2018-02-11 16:15:45 +1000

    Remove duplicate package archive

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -106,7 +106,6 @@
 
 (unless (package-installed-p 'use-package)
   (add-to-list 'package-archives
-               '("gnu" . "https://elpa.gnu.org/packages/")
                '("melpa" . "https://melpa.org/packages/")
                '("melpa-stable" . "https://stable.melpa.org/packages/"))
   (package-install 'use-package))
