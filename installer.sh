c2ac24a81013b3b2a9f77333a5b8362e8805e919
Peter Mosmans <support@go-forward.net>
2020-04-30 08:42:24 +0200

    feat(installer): use generic installation script

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 
 # installer - Basic installer (linker) for various files
 #
-# Copyright (C) 2015-2019 Peter Mosmans
+# Copyright (C) 2015-2020 Peter Mosmans
 #                         <support AT go-forward.net>
 #
 # This program is free software: you can redistribute it and/or modify
@@ -10,56 +10,141 @@
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
+# For configuration, use the INIFILE
+INIFILE=.installerrc
+
 # Define associative arrays
+declare -A copyfiles
 declare -A linkfiles
 declare -A osfiles
 
-# OS-indepentent files to link
-linkfiles=(
-    [.]=".aliases .bashrc .functions .gitconfig .gitignore_global .tmux.conf .tmux.default.theme .tmux.dracula.theme .zshrc"
-    [.emacs.d]="init.el"
-)
 
-# Files to link, depending on OS
-osfiles=(
-    [.]=".minttyrc .zshenv"
-    [.ssh]="config"
-)
 
-## Don't change anything below this line
-VERSION=0.7
+# The file .installerrc should contain all installer-specific files:
+# copyfiles, linkfiles, osfiles and executables.
+
+# copyfiles: Files to copy.
+# An array of relative location from where the script is run, and files.
+#
+# Example:
+# copyfiles=(
+#     [.]=" .versionrc"
+#     [.git/hooks]="pre-commit"
+# )
+#
+# This will copy the file .versionrc to the target folder,
+# and pre-commit to the folder .git/hooks
+
+# linkfiles: OS-independent files to link.
+# An array of relative location from where the script is run, and files.
+#
+# Example:
+# linkfiles=(
+#     [.]=".aliases"
+#     [.emacs.d]="init.el"
+# )
+#
+# This will link the source file .aliases to .aliases in the target folder,
+# and the source file .emacs.d/init.el to .emacs.d/init.el in the target folder
+
+# osfiles: Files to link, depending on OS
+# Source files will be read from within the os_name subdirectory
+#
+# Example:
+# osfiles=(
+#     [.]=".zshenv"
+#     [.ssh]="config"
+# )
+#
+# If executed on GNU-Linux, this will link the source file GNU-Linux/.zshenv to .zshenv in the target folder,
+# and the source file GNU-Linux/.ssh/config to .ssh/config in the target folder.
+
+# executables: Files that should be on the search path and accessible.
+#
+# Example:
+# executables="lint-jenkins"
+#
+# This will search for lint-jenkins, and emits a warning if the file cannot be found/executed.
+# A warning will be shown if any of the files cannot be found (on the search paths)
+
+VERSION=1.0
 
 os=$(uname -o|sed "s/\//-/")
 source=$(dirname $(readlink -f $0))
 target=$1
 
-# Check whether the script is being executed from within the source directory
-if [ -z "$target" ]; then
-    target=$(readlink -f .)
-     if [ "${target}" == "${source}" ]; then
-        echo "Usage: installer.sh [TARGET]"
-        echo "       or run from within target directory"
-        exit
+# Read the settings
+INIFILE="${source}/${INIFILE}"
+echo "Reading settings from ${INIFILE}"
+source ${INIFILE}
+
+usage() {
+    # Check whether the script is being executed from within the source directory
+    if [ -z "$target" ]; then
+        target=$(readlink -f .)
+        if [ "${target}" == "${source}" ]; then
+            echo "Usage: installer.sh [TARGET]"
+            echo "       or run from within target directory"
+            exit
+        fi
     fi
-fi
+}
 
-echo "[*] Installing..."
-
-# Link files
-for targetdirectory in "${!linkfiles[@]}"; do
-    mkdir -p ${target}/${targetdirectory}/ &>/dev/null
-    for file in ${linkfiles[$targetdirectory]}; do
-        ln -fv ${source}/${targetdirectory}/${file} ${target}/${targetdirectory}/${file}
+copy_files() {
+    # Copy files
+    for targetdirectory in "${!copyfiles[@]}"; do
+        mkdir -p ${target}/${targetdirectory}/ &>/dev/null
+        for file in ${copyfiles[$targetdirectory]}; do
+            cp -uv ${source}/${targetdirectory}/${file} ${target}/${targetdirectory}/${file}
+        done
     done
-done
+}
 
-# Link operating-system specific files
-for targetdirectory in "${!osfiles[@]}"; do
-    for file in ${osfiles[$targetdirectory]}; do
-        if [[ -f ${source}/${os}/${file} ]]; then
-            ln -fv ${source}/${os}/${targetdirectory}/${file} ${target}/${targetdirectory}/${file}
+link_files() {
+    # Link files
+    for targetdirectory in "${!linkfiles[@]}"; do
+        mkdir -p ${target}/${targetdirectory}/ &>/dev/null
+        for file in ${linkfiles[$targetdirectory]}; do
+            ln -fv ${source}/${targetdirectory}/${file} ${target}/${targetdirectory}/${file}
+        done
+    done
+    }
+
+link_os_files() {
+    # Link operating-system specific files
+    for targetdirectory in "${!osfiles[@]}"; do
+        for file in ${osfiles[$targetdirectory]}; do
+            if [[ -f ${source}/${os}/${file} ]]; then
+                ln -fv ${source}/${os}/${targetdirectory}/${file} ${target}/${targetdirectory}/${file}
+            fi
+        done
+    done
+    }
+
+check_executables() {
+    # Check if executables are available
+    echo "[*] Checking whether executables can be found..."
+    for executable in ${executables}; do
+        if [[ -z "$(which ${executable} 2>/dev/null)" ]] ; then
+            echo "[!] Could not find ${executable} in paths: Not everything might work correctly"
+            WARNING=1
         fi
     done
-done
+}
 
-echo "[+] Done"
+finish() {
+    if [[ -n "$WARNING" ]]; then
+        echo "[!] Done, but with warnings"
+        exit 1
+    else
+        echo "[+] Done"
+    fi
+}
+
+echo "[*] Installing..."
+usage
+copy_files
+link_files
+link_os_files
+check_executables
+finish
