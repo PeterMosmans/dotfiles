299423f900fad7fc480379d9936a26e8d08bedf3
Peter Mosmans <support@go-forward.net>
2020-05-15 22:56:20 +0200

    feat(zplug): move zplug plugins to separate file

---
+++ GNU-Linux/.zplug--packages.zsh
@@ -0,0 +1,10 @@
+zplug "ael-code/zsh-colored-man-pages"
+zplug "plugins/docker", from:oh-my-zsh
+zplug "plugins/docker-compose", from:oh-my-zsh
+zplug "plugins/git", from:oh-my-zsh
+zplug "plugins/pip", from:oh-my-zsh
+zplug "plugins/sudo", from:oh-my-zsh
+zplug "plugins/tmux", from:oh-my-zsh
+zplug "themes/agnoster", as:theme, from:oh-my-zsh
+zplug "zsh-users/zsh-autosuggestions"
+zplug "zsh-users/zsh-syntax-highlighting" # Should be last
