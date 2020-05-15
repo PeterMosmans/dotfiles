c3045a1fca595595aefb4f4b006eefb099d12940
Peter Mosmans <support@go-forward.net>
2020-05-15 22:59:56 +0200

    feat(zplug): add list of plugins for OS

---
+++ Msys/.zsh--packages.zsh
@@ -0,0 +1,8 @@
+zplug "ael-code/zsh-colored-man-pages"
+zplug "plugins/tmux", from:oh-my-zsh
+zplug "b4b4r07/enhancd", use:init.sh
+zplug "plugins/git", from:oh-my-zsh
+zplug "plugins/sudo", from:oh-my-zsh
+zplug "themes/agnoster", as:theme, from:oh-my-zsh
+zplug "zsh-users/zsh-syntax-highlighting"
+zplug "zsh-users/zsh-autosuggestions"
