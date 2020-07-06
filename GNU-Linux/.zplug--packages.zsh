0908a93f3c331c348d2fb1db05021cdba5c5ef23
Peter Mosmans <support@go-forward.net>
2020-07-06 20:11:31 +0200

    feat: add zsh emacs plugin

--- GNU-Linux/.zplug--packages.zsh
+++ GNU-Linux/.zplug--packages.zsh
@@ -1,6 +1,7 @@
 zplug "ael-code/zsh-colored-man-pages"
 zplug "plugins/docker", from:oh-my-zsh
 zplug "plugins/docker-compose", from:oh-my-zsh
+zplug "PeterMosmans/emacs-zsh", from:github
 zplug "plugins/git", from:oh-my-zsh
 zplug "plugins/pip", from:oh-my-zsh
 zplug "plugins/sudo", from:oh-my-zsh
