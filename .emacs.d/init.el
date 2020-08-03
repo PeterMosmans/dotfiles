ba8e883744afe9c4019cdbdeb2b459b669c9ebd6
Peter Mosmans <support@go-forward.net>
2020-08-03 11:24:35 +0200

    feat(init.el): do not show message when org directory doesn't exist

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -855,9 +855,7 @@
 (global-set-key (kbd "C-<f12>") 'org-clock-out)
 (global-set-key (kbd "M-<f12>") 'my-org-clock-show-list)
 (if (file-exists-p my-org-directory)  ;; use my-org-directory if it exists
-    (setq org-directory my-org-directory)
-  (message "Please specify correct my-org-directory: The directory %s does not exist."
-           my-org-directory))
+    (setq org-directory my-org-directory))
 (org-babel-do-load-languages
  'org-babel-load-languages
  '(
