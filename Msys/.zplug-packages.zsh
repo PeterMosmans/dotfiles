c038e8712e7f45a84a812a26cb8f3dfd6e0e2cca
Peter Mosmans <support@go-forward.net>
2020-10-22 15:32:47 +0200

    feat: add k zsh plugin

--- Msys/.zplug-packages.zsh
+++ Msys/.zplug-packages.zsh
@@ -2,5 +2,6 @@ zplug "ael-code/zsh-colored-man-pages"
 zplug "plugins/git", from:oh-my-zsh
 zplug "themes/agnoster", as:theme, from:oh-my-zsh
 zplug "zsh-users/zsh-autosuggestions"
-zplug "PeterMosmans/emacs-zsh", from:github
+zplug "PeterMosmans/emacs-zsh"
+zplug "supercrabtree/k", use:k.sh
 zplug "zsh-users/zsh-syntax-highlighting"
