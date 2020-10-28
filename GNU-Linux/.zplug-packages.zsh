494360628e7946f98fa19467f640bddbdbde61e8
Peter Mosmans <support@go-forward.net>
2020-10-28 12:02:18 +0100

    feat: add k plugin for zsh

--- GNU-Linux/.zplug-packages.zsh
+++ GNU-Linux/.zplug-packages.zsh
@@ -1,11 +1,12 @@
+zplug "PeterMosmans/emacs-zsh"
 zplug "ael-code/zsh-colored-man-pages"
 zplug "plugins/docker", from:oh-my-zsh
 zplug "plugins/docker-compose", from:oh-my-zsh
-zplug "PeterMosmans/emacs-zsh", from:github
 zplug "plugins/git", from:oh-my-zsh
 zplug "plugins/pip", from:oh-my-zsh
 zplug "plugins/sudo", from:oh-my-zsh
 zplug "plugins/tmux", from:oh-my-zsh
+zplug "supercrabtree/k", use:k.sh
 zplug "themes/agnoster", as:theme, from:oh-my-zsh
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-syntax-highlighting" # Should be last
